a88bb01c28793b26f83c900bc56d9db54f3a9f26
Vitaliy Ryaboy <vriaboy@gmail.com>
2019-03-04 11:05:36 +0100

    update composer and tests configurations

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 define('TEST_RESOURCES_DIR', __DIR__.'/PHPPdf/Resources');
 
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', '1');
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 
